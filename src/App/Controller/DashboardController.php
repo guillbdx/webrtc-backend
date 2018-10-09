b78c4d96ac78c38c7999ec045b58f40e7c6c9bb1
gpedelagrabe <gpedelagrabe@gmail.com>
2018-10-09 21:39:48 +0200

    Dashboard home.

--- src/App/Controller/DashboardController.php
+++ src/App/Controller/DashboardController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Manager\UserManager;
+use App\Service\ShootingStateService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,14 +17,20 @@ class DashboardController extends AbstractController
 {
     /**
      * @Route("/", name="dashboard")
+     * @param ShootingStateService $shootingStateService
      * @param UserInterface|User $user
      * @return Response
      */
     public function dashboard(
+        ShootingStateService $shootingStateService,
         UserInterface $user
     )
     {
-        return $this->render('frontend/dashboard/dashboard.html.twig');
+        $shootingState = $shootingStateService->getUserShootingState($user);
+
+        return $this->render('frontend/dashboard/dashboard.html.twig', [
+            'currentlyShooting' => ShootingStateService::ACTIVE === $shootingState
+        ]);
     }
 
     /**
