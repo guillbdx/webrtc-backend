8247df6f388392e0af3ffe0d1f4aa2665137bb6f
gpedelagrabe <gpedelagrabe@gmail.com>
2018-10-11 20:37:46 +0200

    Refactors cookie alert.

---
+++ src/App/Subscriber/SetCookieAlertSubscriber.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guillaume
+ * Date: 10/11/18
+ * Time: 8:02 PM
+ */
+
+namespace App\Subscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class SetCookieAlertSubscriber implements EventSubscriberInterface
+{
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => 'setCookie'
+        ];
+    }
+
+    /**
+     * @param FilterResponseEvent $event
+     */
+    public function setCookie(FilterResponseEvent $event): void
+    {
+        $request = $event->getRequest();
+        $response = $event->getResponse();
+
+        if (false === $event->isMasterRequest()) {
+            return;
+        }
+        if ($request->headers->has('x-requested-with') && 'XMLHttpRequest' === $request->headers->get('x-requested-with')) {
+            return;
+        }
+
+        if (!$request->cookies->has('cookieAlert')) {
+            $cookie = new Cookie('cookieAlert', 'firstVisit', time() + 3600 * 24 * 365);
+            $response->headers->setCookie($cookie);
+        } else {
+            $cookie = new Cookie('cookieAlert', 'notFirstVisit', time() + 3600 * 24 * 365);
+            $response->headers->setCookie($cookie);
+        }
+    }
+
+}
