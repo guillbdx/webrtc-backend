df1941182a3b80762c26c4a9f0abd8288704b363
gpedelagrabe <gpedelagrabe@gmail.com>
2018-10-14 22:50:36 +0200

    Dashboard menu.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,36 @@
 require('../less/main.less');
+
+//----------------------------------------------------------------
+// DASHBOARD SIDEBAR
+//----------------------------------------------------------------
+
+jQuery(document).ready(function() {
+    function showSidebar() {
+        jQuery("#dashboard-sidebar").show();
+        jQuery('body').addClass('sidebarDisplayed');
+    }
+
+    function hideSidebar() {
+        jQuery("#dashboard-sidebar").hide();
+        jQuery('body').removeClass('sidebarDisplayed');
+    }
+
+    if (jQuery(document).width() < 650) {
+        hideSidebar();
+    } else {
+        showSidebar();
+    }
+
+    jQuery("#menu-toggle").click(function(e) {
+        if (jQuery('#dashboard-sidebar').is(':visible')) {
+            hideSidebar();
+        } else {
+            showSidebar();
+        }
+
+        return false;
+    });
+});
+
+
+
