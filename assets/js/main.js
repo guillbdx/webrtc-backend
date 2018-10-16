e1080fade0dcfb5c241506e078a1d55278b9f238
gpedelagrabe <gpedelagrabe@gmail.com>
2018-10-17 00:11:15 +0200

    Direct link to Shoot if in active state.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 require('../less/main.less');
 
-
+// Tooltips
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-})
+});
+
+// Shoot page redirection
+$(document).ready(function() {
+    jQuery('#linkToShootPage').click(function() {
+        setTimeout(function() {
+            document.location.href = '/dashboard';
+        }, 1000);
+    });
+});
